fee87b511cf36f8c7e9069734872fccb86788ac4
jinfeiliang <jinl33@nycstudents.net>
2025-10-29 14:05:25 -0400

    Basic Operations

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-
 // Basic Operations - Example //
 // if you wish to use for the select menu //
-function handleBasicOperation(type) {
-    const num1;
-    const num2;
+function handleBasicOperation(type, num1, num2) {
+  let result;
 
-    let result;
-    
-    switch (type) {
-        case "add": result = num1 + num2; break;
-       
-        case "divide": result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero"; break;
-        
-        default: result = "Invalid operation";
-    }
+  switch (type) {
+    case "add":
+      result = num1 + num2;
+      break;
 
-    document.getElementById("basicResult").textContent = `Result: ${result}`;
+    case "substract":
+      result = num1 - num2;
+
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+
+    case "divide":
+      result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+      break;
+
+    case "modulus":
+      result = num1 % num2;
+      break;
+
+    default:
+      result = "Invalid operation";
+  }
+
+  document.getElementById("basicResult").textContent = `Result: ${result}`;
 }
+
+document.querySelector("#Add_BTN").addEventListener("click", () => {
+  let X = document.querySelector("#num1").value;
+  let Y = document.querySelector("#num2").value;
+  handleBasicOperation("add", Number(X), Number(Y));
+});
+document.querySelector("#Substract_BTN").addEventListener("click", () => {
+  let X = document.querySelector("#num1").value;
+  let Y = document.querySelector("#num2").value;
+  handleBasicOperation("substract", Number(X), Number(Y));
+});
+document.querySelector("#Multiply_BTN").addEventListener("click", () => {
+  let X = document.querySelector("#num1").value;
+  let Y = document.querySelector("#num2").value;
+  handleBasicOperation("multiply", Number(X), Number(Y));
+});
+document.querySelector("#Divide_BTN").addEventListener("click", () => {
+  let X = document.querySelector("#num1").value;
+  let Y = document.querySelector("#num2").value;
+  handleBasicOperation("divide", Number(X), Number(Y));
+});
+document.querySelector("#Modulus_BTN").addEventListener("click", () => {
+  let X = document.querySelector("#num1").value;
+  let Y = document.querySelector("#num2").value;
+  handleBasicOperation("modulus", Number(X), Number(Y));
+});
 
 // Math Formula Handlers
