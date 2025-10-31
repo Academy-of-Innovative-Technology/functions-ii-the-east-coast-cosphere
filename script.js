db0f082cfea1df17d95cdc7603d63205bf0171a1
jinfeiliang <jinl33@nycstudents.net>
2025-10-31 13:43:20 -0400

    Complete?

--- script.js
+++ script.js
@@ -66,29 +66,29 @@ function Rentangle_Area(Length, Width){
   return Formula
 }
 document.querySelector("#Area_Rectangle_BTN").addEventListener("click", () => {
-  let Length = document.querySelector("#rectLength").value || 1;
-  let Height = document.querySelector("#rectWidth").value || 1;
+  let Length = parseFloat(document.querySelector("#rectLength").value) || 1;
+  let Height = parseFloat(document.querySelector("#rectWidth").value) || 1;
   document.querySelector("#rectResult").innerHTML = Rentangle_Area(Length, Height);
 })
 
 function Area_Circle(Radius){
-  let Formula = Math.PI * Radius^2;
+  let Formula = Math.PI * Radius**2;
   return Formula
 }
 document.querySelector("#Area_Circle_BTN").addEventListener("click", () => {
-  let Radius = document.querySelector("#circleRadius").value || 1;
+  let Radius = parseFloat(document.querySelector("#circleRadius").value) || 1;
   document.querySelector("#circleResult").innerHTML = Area_Circle(Radius);
 })
 
 
 function Pythagorean_Theorem(Side_A, Side_B){
-  let Formula = Math.sqrt(Side_A^2 + Side_B^2);
+  let Formula = Math.sqrt(Side_A**2 + Side_B**2);
   return Formula
 }
 document.querySelector("#Pythagorean_Theorem_BTN").addEventListener("click", () => {
-  let Side_A = document.querySelector("#sideA").value || 1;
-  let Side_B = document.querySelector("#sideB").value || 1;
-  document.querySelector("#pythagoreanResult").innerHTML = Rentangle_Area(Side_A, Side_B);
+  let Side_A = parseFloat(document.querySelector("#sideA").value) || 1;
+  let Side_B = parseFloat(document.querySelector("#sideB").value) || 1;
+  document.querySelector("#pythagoreanResult").innerHTML = Pythagorean_Theorem(Side_A, Side_B);
 })
 
 function Interest(Principal, Rate_Percentage, Years){
@@ -96,21 +96,21 @@ function Interest(Principal, Rate_Percentage, Years){
   return Formula
 }
 document.querySelector("#Interest_BTN").addEventListener("click", () => {
-  let Principal = document.querySelector("#principal").value || 1;
-  let Rate_Percentage = document.querySelector("#rate").value || 1;
-  let Years = document.querySelector("#time").value || 1;
+  let Principal = parseFloat(document.querySelector("#principal").value) || 1;
+  let Rate_Percentage = parseFloat(document.querySelector("#rate").value) || 1;
+  let Years = parseFloat(document.querySelector("#time").value) || 1;
   document.querySelector("#interestResult").innerHTML = Interest(Principal, Rate_Percentage, Years);
 })
 
 
 
 function Power(Base, Exponent){
-  let Formula = Base^Exponent;
+  let Formula = Base ** Exponent;
   return Formula
 }
 document.querySelector("#Power_BTN").addEventListener("click", () => {
-  let Base = document.querySelector("#base").value || 1;
-  let Exponent = document.querySelector("#exponent").value || 1;
+  let Base = parseFloat(document.querySelector("#base").value) || 1;
+  let Exponent = parseFloat(document.querySelector("#exponent").value) || 1;
   document.querySelector("#powerResult").innerHTML = Power(Base, Exponent);
 })
 
@@ -120,8 +120,8 @@ function Perimeter_Rectangle(Length, Width){
   return Formula
 }
 document.querySelector("#Perimeter_Rectangle_BTN").addEventListener("click", () => {
-  let Length = document.querySelector("#periLength").value || 1;
-  let Width = document.querySelector("#periWidth").value || 1;
+  let Length = parseFloat(document.querySelector("#periLength").value) || 1;
+  let Width = parseFloat(document.querySelector("#periWidth").value) || 1;
   document.querySelector("#perimeterResult").innerHTML = Perimeter_Rectangle(Length, Width);
 })
 
@@ -130,37 +130,49 @@ function Circumference_Circle(Radius){
   let Formula = 2 * Math.PI * Radius;
   return Formula
 }
-/* --- */
+document.querySelector("#Circumference_Circle_BTN").addEventListener("click", () => {
+  let Radius = parseFloat(document.querySelector("#circRadius").value) || 1;
+  document.querySelector("#circumferenceResult").innerHTML = Circumference_Circle(Radius);
+})
 
 function Area_Triangle(Base, Height){
   let Formula = (Base * Height) / 2;
   return Formula
 }
 document.querySelector("#Area_Triangle_BTN").addEventListener("click", () => {
-  let Base = document.querySelector("#triangleBase").value || 1;
-  let Height = document.querySelector("#triangleHeight").value || 1;
+  let Base = parseFloat(document.querySelector("#triangleBase").value) || 1;
+  let Height = parseFloat(document.querySelector("#triangleHeight").value) || 1;
   document.querySelector("#triangleAreaResult").innerHTML = Area_Triangle(Base, Height);
 })
 
 function Volume_Cube(Side){
-  let Formula = Side^3;
+  let Formula = Side**3;
   return Formula
 }
-/* --- */
+document.querySelector("#Volume_Cube_BTN").addEventListener("click", () => {
+  let Side = parseFloat(document.querySelector("#cubeSide").value) || 1;
+  document.querySelector("#cubeVolumeResult").innerHTML = Volume_Cube(Side);
+})
+
 
 function Volume_Prism(Length, Width, Height){
   let Formula = Length * Width * Height;
   return Formula
 }
-/* --- */
+document.querySelector("#Volume_Prism_BTN").addEventListener("click", () => {
+  let Length = parseFloat(document.querySelector("#prismLength").value) || 1;
+  let Width = parseFloat(document.querySelector("#prismWidth").value) || 1;
+  let Height = parseFloat(document.querySelector("#prismHeight").value) || 1;
+  document.querySelector("#prismVolumeResult").innerHTML = Volume_Prism(Length, Width, Height);
+})
 
 function Volume_Cylinder(Radius, Height){
-  let Formula = Math.PI * (Radius^2) * Height;
+  let Formula = Math.PI * (Radius**2) * Height;
   return Formula
 }
 document.querySelector("#Volume_Cylinder_BTN").addEventListener("click", () => {
-  let Radius = document.querySelector("#cylRadius").value || 1;
-  let Height = document.querySelector("#cylHeight").value || 1;
+  let Radius = parseFloat(document.querySelector("#cylRadius").value) || 1;
+  let Height = parseFloat(document.querySelector("#cylHeight").value) || 1;
   document.querySelector("#triangleAreaResult").innerHTML = Volume_Cylinder(Radius, Height);
 })
 
@@ -169,8 +181,8 @@ function Calculate_Speed(Distance, Time){
   return Formula
 }
 document.querySelector("#Calculate_Speed_BTN").addEventListener("click", () => {
-  let Distance = document.querySelector("#distance").value || 1;
-  let Time = document.querySelector("#time").value || 1;
+  let Distance = parseFloat(document.querySelector("#distance").value) || 1;
+  let Time = parseFloat(document.querySelector("#time").value) || 1;
   document.querySelector("#speedResult").innerHTML = Calculate_Speed(Distance, Time);
 })
 
@@ -180,32 +192,32 @@ function Density(Mass, Volume){
   return Formula
 }
 document.querySelector("#Density_BTN").addEventListener("click", () => {
-  let Mass = document.querySelector("#mass").value || 1;
-  let Volume = document.querySelector("#volume").value || 1;
+  let Mass = parseFloat(document.querySelector("#mass").value) || 1;
+  let Volume = parseFloat(document.querySelector("#volume").value) || 1;
   document.querySelector("#densityResult").innerHTML = Density(Mass, Volume);
 })
 
 
 function BMI(Weight, Height){
-  let Formula = Weight / (Height^2);
+  let Formula = Weight / (Height**2);
   return Formula
 }
 document.querySelector("#BMI_BTN").addEventListener("click", () => {
-  let Weight = document.querySelector("#weight").value || 1;
-  let Height = document.querySelector("#height").value || 1;
-  document.querySelector("#bmiResult").innerHTML = Density(Weight, Height);
+  let Weight = parseFloat(document.querySelector("#weight").value) || 1;
+  let Height = parseFloat(document.querySelector("#height").value) || 1;
+  document.querySelector("#bmiResult").innerHTML = BMI(Weight, Height);
 })
 
 
 function Discriminant(A,B,C){
-  let Formula = (B^2) - 4(A)(C);
+  let Formula = (B**2) - 4(A)(C);
   return Formula
 }
 document.querySelector("#Discriminant_BTN").addEventListener("click", () => {
-  let A = document.querySelector("#a").value || 1;
-  let B = document.querySelector("#b").value || 1;
-  let C = document.querySelector("#c").value || 1;
-  document.querySelector("#discriminantResult").innerHTML = Density(A, B, C);
+  let A = parseFloat(document.querySelector("#a").value) || 1;
+  let B = parseFloat(document.querySelector("#b").value) || 1;
+  let C = parseFloat(document.querySelector("#c").value) || 1;
+  document.querySelector("#discriminantResult").innerHTML = Discriminant(A, B, C);
 })
 
 
